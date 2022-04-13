f9b5398ae0fdd64b7e077a5b62f009ec78c89701
elegentin(xiangyu xie) <nosnickers@gmail.com>
2022-04-13 16:48:28 +0800

    feat: 添加flat解法

--- js/m_flat.js
+++ js/m_flat.js
@@ -1,3 +1,19 @@
 // 手写array flat
-// [1, 2, 3, 4, [5, 6]]
-m_flat([1, 2, 3, [4, [5, 6]]]);
+// [1, 2, 3, 4, [5, 6,]]
+
+function m_flat(list, depth = 1) {
+  if (depth === 0) {
+    return list;
+  }
+  return list.reduce((sum, cur) => {
+    if (!Array.isArray(cur) && !cur.length) {
+      sum = [...sum, cur];
+    } else {
+      sum = [...sum, ...m_flat(cur, depth-1)];
+    }
+    console.log(333333, sum)
+    return sum;
+  }, []);
+}
+
+console.log(m_flat([1, 2, 3, [4, [5, 6, [7, [8]]]]], 2))
